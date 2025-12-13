2f151cdeb79137ad95fa682137a3fc7e147fb823
Sami Serhan <sami19.serhan@gmail.com>
2025-12-13 13:19:40 +0200

    Format the code and fix the bug

--- federated_learning/fl_job.py
+++ federated_learning/fl_job.py
@@ -5,15 +5,23 @@ from nvflare.job_config.script_runner import ScriptRunner
 import os
 import argparse
 from split_data import split_cifar10_data
-from nvflare.job_config.api import FedJob
 
-def federated_learning_arg_parser()-> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Federated Learning with NVFlare and PyTorch")
-    parser.add_argument(
-        "--n_clients", type=int, default=4, help="Number of clients to participate in federated learning"
+
+def federated_learning_arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Federated Learning with NVFlare and PyTorch"
     )
     parser.add_argument(
-        "--num_rounds", type=int, default=2, help="Number of communication rounds for federated learning"
+        "--n_clients",
+        type=int,
+        default=4,
+        help="Number of clients to participate in federated learning",
+    )
+    parser.add_argument(
+        "--num_rounds",
+        type=int,
+        default=2,
+        help="Number of communication rounds for federated learning",
     )
     return parser.parse_args()
 
@@ -26,7 +34,12 @@ def runner():
     split_cifar10_data(num_clients=n_clients)
     train_script = "federated_learning/client.py"
 
-    job = FedAvgJob(name="fedavg", n_clients=n_clients, num_rounds=num_rounds, initial_model=get_resnet50_model())
+    job = FedAvgJob(
+        name="fedavg",
+        n_clients=n_clients,
+        num_rounds=num_rounds,
+        initial_model=get_resnet50_model(),
+    )
     print("FedAvgJob created.")
     # Add clients
     for i in range(n_clients):
@@ -38,10 +51,9 @@ def runner():
 
     file_path = os.path.abspath(__file__)
     workspace_path = os.path.dirname(os.path.dirname(file_path)) + "/workspace"
-    job_config_dir = "./workspace/"
-    print("job-config is at ", os.path.join(job_config_dir, "fedavg"))
-
+    print("job-config is at ", workspace_path)
     job.simulator_run(workspace=workspace_path)
+
 
 if __name__ == "__main__":
     runner()
--- federated_learning/split_data.py
+++ federated_learning/split_data.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch.utils.data import random_split
 import torchvision.datasets as datasets
@@ -7,7 +6,9 @@ import torchvision.transforms as transforms
 import re
 from torch.utils.data import DataLoader, Subset
 
-data_path = './data'
+data_path = "./data"
+
+
 def split_cifar10_data(num_clients: int = 4):
     """Splits CIFAR-10 data among a specified number of clients deterministically.
 
@@ -17,20 +18,28 @@ def split_cifar10_data(num_clients: int = 4):
         tuple[list[DataLoader], DataLoader]: List of client DataLoaders, and test DataLoader.
     """
     # Transforms
-    transform_train = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
+    transform_train = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+    transform_test = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
 
     # Load datasets
-    train_dataset = datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform_train)
-    test_dataset  = datasets.CIFAR10(root=data_path, train=False, download=True, transform=transform_test)
+    train_dataset = datasets.CIFAR10(
+        root=data_path, train=True, download=True, transform=transform_train
+    )
+    test_dataset = datasets.CIFAR10(
+        root=data_path, train=False, download=True, transform=transform_test
+    )
 
     # Deterministic split
     N = len(train_dataset)
@@ -49,8 +58,9 @@ def split_cifar10_data(num_clients: int = 4):
 
     return client_loaders, test_loader
 
-def get_client_data_loader(client_id: str, num_clients)-> tuple[DataLoader, DataLoader]:
+
+def get_client_data_loader(client_id: str, num_clients) -> tuple[DataLoader, DataLoader]:
     client_loaders, test_loader = split_cifar10_data(num_clients=num_clients)
-    indx = int(re.search(r'site-(\d+)', client_id).group(1)) - 1
+    indx = int(re.search(r"site-(\d+)", client_id).group(1)) - 1
     print(indx)
     return client_loaders[indx], test_loader
